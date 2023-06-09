bfaa1e10b7ee3fa599956330fe7a873f222bbfdc
TAHiRLI <tahrli.tahirr@gmail.com>
2023-06-09 15:18:12 +0400

    done

---
+++ ConsoleApp1/Group.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Group
+    {
+        public string Name;
+        public int studentCount;
+        public Student[] students;
+
+        public void AddStuden(Student telebe)
+        {
+           Array.Resize(ref students, students.Length +1);
+            students[students.Length-1] = telebe;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Course code = new Course();
+            code.Name = "Code";
+
+         
+
+
+            Group P235 = new Group();
+            P235.students = new Student[0];
+
+
+
+            Student ferid = new Student();
+            Student tahir = new Student();
+
+            ferid.Fullname = "Ferid hadizade";
+            tahir.Fullname = "Tahir Tahirli";
+
+
+
+            P235.AddStuden(ferid);
+            P235.AddStuden(tahir);
+
+
+            foreach (var stud in P235.students)
+            {
+                Console.WriteLine(stud.Fullname);
+            }
+
+
+        }
+    }
+}
